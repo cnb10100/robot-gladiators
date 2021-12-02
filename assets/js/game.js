97d79b0aa75b1e48f65ec0c5af97b22ecc8b1f78
Cysmi <cnb10100@gmail.com>
2021-12-02 02:29:12 -0500

    added high-scores to localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -215,12 +215,20 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-  } else {
-    window.alert("You've lost your robot in battle!");
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
@@ -228,7 +236,8 @@ var endGame = function() {
 
   if (playAgainConfirm) {
     startGame();
-  } else {
+  }
+  else {
     window.alert('Thank you for playing Robot Gladiators! Come back soon!');
   }
 };
